05bef3a4290cc4e6763bf02f094846bacc0b22c1
Adenike <adeyinkaadenike992@gmail.com>
2024-03-13 20:24:08 +0100

    commited today

--- Page/LipPage.js
+++ Page/LipPage.js
@@ -1,19 +1,47 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ImageBackground, PixelRatio } from 'react-native';
 import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { useNavigation } from '@react-navigation/native';
+
 
 
 const LipPage = ({ route }) => {
-  const { imageUri } = route.params;
+ 
+  const fontScale = PixelRatio.getFontScale();
+  const getFontSize = (size) => size / fontScale;
+  const { item } = route.params;
+  const navigation = useNavigation();
+
+  const handlePressMessage = () => {
+    // Navigate to the message page
+    navigation.navigate('History');
+  };
+
   return (
     <View style={styles.container}>
      <View style={{width:"100%", height:"30%"}}>
      <ImageBackground
-         source={{ uri: imageUri }} 
+         source={item.image}
         style={styles.image}
-      >
-      
-      </ImageBackground>
+      />
+
+      </View>
+      <View>
+     <View style={{marginTop:"5%", paddingHorizontal:"5%", display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-between"}}>
+     <View style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
+        <TouchableOpacity style={styles.cirle}>
+        <Image source={item.images} style={styles.image}/>
+        </TouchableOpacity>
+      <View style={{ marginHorizontal: "5%" }}>
+      <Text style={{ fontSize: getFontSize(25) , marginBottom:"3%", fontWeight:"900",}}>{item.name}</Text>
+      <Text style={{ fontSize: getFontSize(15) , marginBottom:"3%", fontWeight:"400",color:'gray'}}>{item.year}</Text>
+      </View>
+      </View>
+      <TouchableOpacity onPress={handlePressMessage}>
+      <AntDesign name="message1" size={24} color="purple" />
+      </TouchableOpacity>
+     </View>
+     
      </View>
     </View>
   );
@@ -35,6 +63,11 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     borderRadius: 50,
   },
+  cirle: {
+    width: 50,
+    height : 50,
+    borderRadius : 25,
+  }
 });
 
 
